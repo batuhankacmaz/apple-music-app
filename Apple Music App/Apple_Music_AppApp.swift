9f0554d3bc331e6ed525132b66aba0b775978a20
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-22 19:02:58 +0300

    Initial Commit

---
+++ Apple Music App/Apple_Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_AppApp.swift
+//  Apple Music App
+//
+//  Created by Batuhan Kacmaz on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
